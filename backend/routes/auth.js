3b74319211db998010233ce31cb4b0af6e5f144c
neelgoyani702 <103193259+neelgoyani702@users.noreply.github.com>
2022-12-09 10:35:21 +0530

    Video 49 Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const JWT_SECRET = 'Harryisagoodb$oy';
 
 // Creat a User Using : POST "/api/auth/createuser". No login Required
 router.post('/createuser', [
@@ -15,24 +20,37 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check whether the user with this email exists already 
     try {
+        // Check whether the user with this email exists already 
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exist" });
         }
+        const salt = await bcrypt.genSalt(10);
+        secPass = await bcrypt.hash(req.body.password, salt);
         // Create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
 
         // .then(user => res.json(user)).
         // catch(err => {console.log(err)
         //     res.json({error: 'Please enter a unique value for email',message: err.message})
         // });
-        res.json(user);
+
+
+        // Create a JWT token and return
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET); // JWT_SECRET is a string
+        // res.json(user);
+        res.json({authToken});
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Internal Server Error');
