5a45e0cd22f2115dbc18d19bab83dcdf3ddb0933
neelgoyani702 <103193259+neelgoyani702@users.noreply.github.com>
2022-12-09 15:36:51 +0530

    Video 52 Completed

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title:{
         type: String,
         required: true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const fetchUser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
 
-router.get('/', (req, res) => {
-    res.send([]);
+const { body, validationResult } = require('express-validator');
+
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
+
+// ROUTES 1: Fetch All Notes using: GET "/api/notes/fetchallnotes". Login Required
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+    // res.send([]);
 });
+
+// ROUTES 2: Add a new Note using: POST "/api/notes/addnote". Login Required
+router.post('/addnote', fetchUser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // If any field is empty
+    if (!title || !description || !tag) {
+        return res.status(400).json({ error: "Please fill all the fields" });
+    }
+    try {
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
 
 module.exports = router;
