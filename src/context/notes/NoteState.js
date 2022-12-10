bc262d6f64a94166954d7e0ce76bc57e346f9fbc
neelgoyani702 <103193259+neelgoyani702@users.noreply.github.com>
2022-12-10 14:31:54 +0530

    Video 58 Completed

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+    const s1 = {
+        name: "Shubham",
+        work: "Web Developer"
+    }
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({ name: "Harry", work: "Web Developer" })
+        }, 1000);
+    }
+
+    return (
+        <NoteContext.Provider value={{ state, update }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
